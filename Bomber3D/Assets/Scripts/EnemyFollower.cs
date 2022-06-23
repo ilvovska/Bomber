8e40d2543564f3b51804ef808e2eefbe37768984
Lirik <anna.yahnyukova@gmail.com>
2022-06-23 16:51:59 +0300

    Fixed follower

--- Bomber3D/Assets/Scripts/EnemyFollower.cs
+++ Bomber3D/Assets/Scripts/EnemyFollower.cs
@@ -3,15 +3,15 @@
 public class EnemyFollower : Enemy
 {
     [SerializeField] private Movement movement;
-    
+
     private Transform player;
 
     private void Update()
     {
         if (movement.IsMoving) return;
-        
-        var direction = (transform.position - player.position).normalized;
-        movement.Move(Direction.Vector2ToDirection(direction));
+
+        var direction = (player.position - transform.position).normalized;
+        movement.Move(Direction.Vector2ToDirection(new Vector2(direction.x, direction.z)));
     }
 
     public void SetPlayerTransform(Transform playerTransform) => player = playerTransform;
